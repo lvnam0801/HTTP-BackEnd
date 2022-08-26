2ca77e7f9faac65c58c9a532d0c22724a2bd191e
lvnam0801 <lvnam0801@gmail.com>
2022-08-26 23:10:10 +0700

    Create User Entity

---
+++ app/src/main/java/demo/backend/entity/User.java
@@ -0,0 +1,18 @@
+package demo.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    private Integer userId;
+    private String name;
+    private Integer age;
+}
