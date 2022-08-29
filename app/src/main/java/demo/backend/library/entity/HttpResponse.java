3eb1d6b0d6eef991998bdd0978b03ab858ca1ac6
lvnam0801 <lvnam0801@gmail.com>
2022-08-29 22:40:28 +0700

    Create Http Respose Class

---
+++ app/src/main/java/demo/backend/library/entity/HttpResponse.java
@@ -0,0 +1,21 @@
+package demo.backend.library.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HttpResponse extends BaseEntity{
+     /**
+     * 0 for successs
+     * 1 for no data found
+     * -1 for failed
+    */
+    public Integer code; // status line of response
+    public Object data; // data of request
+    public String message; // annotate for resulut of request or give a notification
+}
