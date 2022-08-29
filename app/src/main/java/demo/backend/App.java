18d646a4badbb6ad538a51020698cfffc09aaa48
lvnam0801 <lvnam0801@gmail.com>
2022-08-29 23:33:32 +0700

    Update Sington Pattern for UserModel

--- app/src/main/java/demo/backend/App.java
+++ app/src/main/java/demo/backend/App.java
@@ -24,30 +24,26 @@ public class App {
          */
         get("/read", (req, res) -> {
             Integer userId = Integer.parseInt(req.queryParams("userId"));
-            UserModel userModel = new UserModel();
-            return userModel.read(userId);
+            return UserModel.getInst().read(userId);
         });
 
         post("/create", (req, res) -> {
             Integer userId = Integer.parseInt(req.queryParams("userId"));
             String name = req.queryParams("name");
             Integer age = Integer.parseInt(req.queryParams("age"));
-            UserModel userModel = new UserModel();
-            return userModel.create(userId, name, age);
+            return UserModel.getInst().create(userId, name, age);
         });
 
         put("/update", (req, res) -> {
             Integer userId = Integer.parseInt(req.queryParams("userId"));
             String name = req.queryParams("name");
             Integer age = Integer.parseInt(req.queryParams("age"));
-            UserModel userModel = new UserModel();
-            return userModel.update(userId, name, age);
+            return UserModel.getInst().update(userId, name, age);
         });
         
         delete("/delete", (req, res) -> {
             Integer userId = Integer.parseInt(req.queryParams("userId"));
-            UserModel userModel = new UserModel();
-            return userModel.delete(userId); 
+            return UserModel.getInst().delete(userId); 
         });
     }
 }
